379650c8997c4306edb58156ca9a6e540d23c0c0
rarahojae20 <rarahojae20@naver.com>
2023-12-22 15:50:22 +0900

    test1, girrafe 오류 해결

--- src/api/tests/test1.ts
+++ src/api/tests/test1.ts
@@ -7,7 +7,9 @@ import { Fruit } from "../../types/fruits";
 
 const Test1: Fruit[] = [
     { apple: 15, strawberry: 20, banana: 6, orange: 10 },
-  ];
+    { apple: 15, strawberry: 20, banana: 6, orange: 10 },
+    { apple: 15, strawberry: 20, banana: 6, orange: 10 },
+];
   
   const elephantTest1 = new Elephant("Elephant", 1);
   const giraffeTest1 = new Giraffe("Giraffe", 1);
@@ -17,7 +19,7 @@ const Test1: Fruit[] = [
   const giraffeFeedings = giraffeTest1.eat(Test1);
   const monkeyFeedings = monkeyTest1.eat(Test1);
   
-  console.log(`코끼리(${elephantTest1.quantity}마리) ${elephantFeedings}번 먹어야 합니다.`);
-  console.log(`기린(${giraffeTest1.quantity}마리) ${giraffeFeedings}번 먹어야 합니다.`);
+  console.log(`코끼리(${elephantTest1.quantity}마리)는 ${elephantFeedings}번 먹어야 합니다.`);
+  console.log(`기린(${giraffeTest1.quantity}마리)는 ${giraffeFeedings}번 먹어야 합니다.`);
   console.log(`원숭이(${monkeyTest1.quantity}마리)는 ${monkeyFeedings}번 먹어야 합니다.`);
   
